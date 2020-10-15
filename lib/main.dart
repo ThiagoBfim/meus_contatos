be649365e9193c3c5d8f874b0d4b340fc8e14d99
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-10-14 21:38:51 -0300

    criação do formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,6 +29,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   ContatoModel contato = new ContatoModel();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +37,74 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(contato.nome ?? ''),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Wrap(
+            spacing: 20, // to apply margin in the main axis of the wrap
+            runSpacing: 10, // to apply margin in the cross axis of the wrap
+            children: <Widget>[
+              TextFormField(
+                validator: nomeValidator(),
+                onChanged: updateNome,
+                decoration: InputDecoration(labelText: "Nome"),
+                maxLength: 100,
+              ),
+              TextFormField(
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  TelefoneInputFormatter(),
+                ],
+                keyboardType: TextInputType.number,
+                onChanged: updateTelefone,
+                decoration: InputDecoration(labelText: "Celular"),
+              ),
+              TextFormField(
+                validator: emailValidator(),
+                onChanged: updateEmail,
+                decoration: InputDecoration(labelText: "E-mail"),
+              ),
+              TextFormField(
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CpfInputFormatter(),
+                ],
+                onChanged: updateCpf,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "CPF"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      print(contato);
+                    }
+                  },
+                  child: Text('Salvar'))
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void updateCpf(cpf) => contato.cpf = cpf;
+
+  void updateEmail(email) => contato.email = email;
+
+  void updateTelefone(telefone) => contato.telefone = telefone;
+
+  void updateNome(nome) => contato.nome = nome;
+
+  TextFieldValidator emailValidator() {
+    return EmailValidator(errorText: 'e-mail inválido');
+  }
+
+  FieldValidator nomeValidator() {
+    return MultiValidator([
+      RequiredValidator(errorText: 'campo obrigatório'),
+      MinLengthValidator(4, errorText: 'tamanho mínimo de 4 caracteres'),
+    ]);
   }
 }
 
@@ -49,6 +114,11 @@ class ContatoModel {
   String cpf;
   String telefone;
   ContatoType tipo;
+
+  @override
+  String toString() {
+    return '{nome: $nome, email: $email, cpf: $cpf, telefone: $telefone, tipo: $tipo}';
+  }
 }
 
 enum ContatoType { CELULAR, TRABALHO, FAVORITO, CASA }
